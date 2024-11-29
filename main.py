64f4aa0955608b7c80f698280ba668bace8b320e
Ding-Jeremy <145600655+Ding-Jeremy@users.noreply.github.com>
2024-11-29 09:52:47 +0100

    Creation elements principaux

---
+++ main.py
@@ -0,0 +1,27 @@
+#
+#
+#
+
+
+class Brick:
+    """Commentaire de classe"""
+
+    def __init__(self):
+        pass
+
+    def show(self):
+        """Commentaire de fonction"""
+        pass
+
+
+class Brick_field:
+    def __init__(self):
+        pass
+
+    def show(self):
+        pass
+
+
+class Breakout:
+    def __init__(self):
+        pass
